c219291bdacaa2bcec5d2585e67d40dd5dde9bc5
tracer-ics2023 <tracer@njuics.org>
2023-10-14 21:18:32 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:18:32 up 23:47,  1 user,  load average: 1.37, 1.15, 1.10

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -30,6 +30,10 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  if(strcmp("pc",s)==0){
+    *success=true;
+    return cpu.pc;
+  }
   for(int i=0;i<32;i++){
     if(strcmp(regs[i],s)==0){
       *success = true;
