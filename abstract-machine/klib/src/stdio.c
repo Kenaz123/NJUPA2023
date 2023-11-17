74256e15a4f2164f77eba740116e3f21165f440b
tracer-ics2023 <tracer@njuics.org>
2023-11-17 23:04:27 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:04:27 up  2:59,  2 users,  load average: 0.15, 0.18, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,70 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+//static char buf[1024];
+static int itoa(int n,char *s,int base){
+  assert(base <= 16);
+  int i = 0,sign = n,bit;
+  if(sign < 0) {
+    n=-n;
+    s[i++]='-';
+  }
+  do{
+    bit = n % base;
+    if(bit >= 10) s[i++] = 'a'+ bit - 10;
+    else s[i++] = '0' + bit; 
+  }while((n/=base)>0);
+  s[i] = '\0';
+  return i;
+
+}
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+ // va_list ap;
+ // int n;
+  //va_start(ap,fmt);
+  //n = vsprintf(buf,fmt,ap);
+  //va_end(ap);
+  //return n;
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap,fmt);
+  char *start = out;
+  for(;*fmt != '\0';++fmt){
+    if(*fmt != '%'){
+      *out = *fmt;
+      ++out;
+    }
+    else{
+      switch(*(++fmt)){
+        case '%':*out=*fmt;++out;break;
+        case 'd':out+=itoa(va_arg(ap,int),out,10);break;
+        case 's':{
+          char *s = va_arg(ap,char*);
+          strcpy(out,s);
+          out+=strlen(out);
+          break;
+        }
+      }
+    }
+  }
+  *out = '\0';
+  va_end(ap);
+  return out - start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  panic("Not Implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  panic("Not Implemented");
 }
 
 #endif
