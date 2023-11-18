07811823fc2a8658e6d8151f5ee6b7369aa6c089
tracer-ics2023 <tracer@njuics.org>
2023-11-19 01:00:36 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:00:36 up 1 day,  4:56,  5 users,  load average: 0.45, 0.41, 0.32

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,10 +21,12 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = ARRLEN(cpu.gpr);
   for(int i = 0; i < reg_num; i++){
     if(ref_r->gpr[i] != cpu.gpr[i]){
+    Log("At %s -> REF: " FMT_WORD ", DUT: " FMT_WORD, reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
       return false;
     }
   }
   if(ref_r->pc != cpu.pc){
+    Log("At pc -> REF: " FMT_WORD ", DUT: " FMT_WORD, ref_r->pc, cpu.pc);
     return false;
   }
   return true;
