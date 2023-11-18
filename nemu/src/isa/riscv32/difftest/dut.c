717256e4be1b6c704e3fbdaea7467f16fe66c28f
tracer-ics2023 <tracer@njuics.org>
2023-11-19 00:33:08 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:33:08 up 1 day,  4:28,  5 users,  load average: 0.49, 0.25, 0.16

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,16 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  int reg_num = ARRLEN(cpu.gpr);
+  for(int i = 0; i < reg_num; i++){
+    if(ref_r->gpr[i] != cpu.gpr[i]){
+      return false;
+    }
+  }
+  if(ref_r->pc != cpu.pc){
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
