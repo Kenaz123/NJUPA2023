ebc218545421f5163ff9669eec5296cd991e7a3c
tracer-ics2023 <tracer@njuics.org>
2023-12-13 20:45:58 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:58 up 1 day,  5:00,  8 users,  load average: 0.44, 0.46, 0.39

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1: printf("event ID = %d", e.event);
+    case 1: printf("event ID = %d\n", e.event);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
