668abb055fe833ed92c34dc0e546ab136afb275f
tracer-ics2023 <tracer@njuics.org>
2023-12-18 17:22:18 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:18 up 6 days,  1:36,  9 users,  load average: 0.79, 0.67, 0.45

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case 0: c->GPRx=0;halt(c->GPRx);//SYS_exit
-    case 1: yield();//SYS_yield
+    case 1: yield();break;//SYS_yield
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
