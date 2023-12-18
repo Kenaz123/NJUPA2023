5fbbf56cd971053bfa447d2233bc5b78906b4f33
tracer-ics2023 <tracer@njuics.org>
2023-12-18 17:24:08 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:24:08 up 6 days,  1:38,  9 users,  load average: 0.77, 0.70, 0.49

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,7 +19,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  if(NO == -1 || NO == 1){
+  if(NO == -1 || NO == 0){
     epc += 4;
   }
   cpu.csr.mcause = NO;
