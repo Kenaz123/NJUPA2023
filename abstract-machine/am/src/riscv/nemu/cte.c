eec0824f52f120ddc6d0c2923e7a02c1ee26eaf1
tracer-ics2023 <tracer@njuics.org>
2023-12-19 17:38:14 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:14 up 7 days,  1:52, 10 users,  load average: 0.35, 0.34, 0.33

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ Context* __am_irq_handle(Context *c) {
     c->mepc+=4;
     switch (c->mcause) {
       case -1: ev.event = EVENT_YIELD; break;
-      case 1: ev.event = EVENT_SYSCALL; break;
+      case 1:case 4: ev.event = EVENT_SYSCALL; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
