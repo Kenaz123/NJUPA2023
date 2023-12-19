69cc232d5a87eb4df6590efc025267119133db2f
tracer-ics2023 <tracer@njuics.org>
2023-12-20 00:14:06 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:14:06 up 7 days,  8:28, 10 users,  load average: 0.42, 0.34, 0.45

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -7,7 +7,7 @@ int main() {
   volatile int j = 0;
   while (1) {
     j ++;
-    if (j == 3) {
+    if (j == 1) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
