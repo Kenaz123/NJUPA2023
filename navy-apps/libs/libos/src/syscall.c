8fd864c11afb5fd4c4df91c8b52d6990f8e6d6bb
tracer-ics2023 <tracer@njuics.org>
2023-12-19 22:51:52 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:52 up 7 days,  7:06, 10 users,  load average: 0.32, 0.41, 0.43

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -56,7 +56,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t _gpr4 asm (GPR4) = a2;
   register intptr_t ret asm (GPRx);
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
-  Log("_syscall_(%d,%d,%d,%d);\n",type,a0,a1,a2);
+  printf("_syscall_(%d,%d,%d,%d);\n",type,a0,a1,a2);
   return ret;
 }
 
