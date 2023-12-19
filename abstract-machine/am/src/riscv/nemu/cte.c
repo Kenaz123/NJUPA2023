85a1046eebbe7daa9d670e09820e20e4dc1b9544
tracer-ics2023 <tracer@njuics.org>
2023-12-19 17:38:51 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:51 up 7 days,  1:53, 10 users,  load average: 0.86, 0.46, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("c->mcause=%d\n",c->mcause);
+    //printf("c->mcause=%d\n",c->mcause);
     c->mepc+=4;
     switch (c->mcause) {
       case -1: ev.event = EVENT_YIELD; break;
