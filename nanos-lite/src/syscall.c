144e5bc23eb942437858179e77cd1d0e95c93eb8
tracer-ics2023 <tracer@njuics.org>
2023-12-22 11:45:38 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:38 up 9 days, 20:00, 10 users,  load average: 0.24, 0.26, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include <common.h>
 #include "syscall.h"
-#define STRACE
+//#define STRACE
 /*int sys_write(int fd,void *buf,size_t count){
   if(fd == 1 || fd == 2){
     for(int i = 0; i < count; i++){
