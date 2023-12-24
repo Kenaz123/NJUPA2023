f2e1303b6f42230f7324c878891b17b07913a763
tracer-ics2023 <tracer@njuics.org>
2023-12-24 18:31:09 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:31:09 up 12 days,  2:45, 10 users,  load average: 1.03, 0.49, 0.29

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
   Log("Initializing processes...");
-  const char filename[] = "/bin/menu";
+  const char filename[] = "/bin/nterm";
   naive_uload(NULL,filename);
   // load program here
 
--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -11,7 +11,7 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 }
 
 uint32_t SDL_GetTicks() {
-  return 0;
+  return NDL_GetTicks();//warn(wrap for 49 days);
 }
 
 void SDL_Delay(uint32_t ms) {
