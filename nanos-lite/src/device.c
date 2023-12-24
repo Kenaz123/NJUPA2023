2c700dcd874166127bc050dfcffe286667c678c9
tracer-ics2023 <tracer@njuics.org>
2023-12-25 02:43:36 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:43:36 up 12 days, 10:58, 11 users,  load average: 0.08, 0.33, 0.57

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,8 +29,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   //memset(temp_buf,0,TEMP_BUFSIZE);
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  /*if(ev.keycode == AM_KEY_NONE) return 0;
-  const char *name = keyname[ev.keycode];
+  if(ev.keycode == AM_KEY_NONE) return 0;
+  /*const char *name = keyname[ev.keycode];
   int ret = ev.keydown ? sprintf(temp_buf,"kd %s\n",name) : sprintf(temp_buf,"ku %s\n",name);
   if(ret >= len){
     strncpy(buf,temp_buf,len-1);
