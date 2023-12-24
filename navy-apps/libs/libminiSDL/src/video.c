32ca322bdcfe1232b70ca5e00a33354523182ee9
tracer-ics2023 <tracer@njuics.org>
2023-12-25 02:36:24 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:36:24 up 12 days, 10:50, 11 users,  load average: 0.67, 0.95, 0.81

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -100,6 +100,8 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  assert(s);
+  assert(s->format);
   if(s->format->palette == NULL) {//32 bits
   if(x == 0 && y == 0 && w == 0 && h == 0) {
     NDL_DrawRect((uint32_t *)s->pixels,x,y,s->w,s->h);
