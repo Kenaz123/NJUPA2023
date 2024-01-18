a05650e9a371fa6635b8caf594bdb49a3759eedd
tracer-ics2023 <tracer@njuics.org>
2024-01-18 15:57:20 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:20 up 3 days,  3:26, 12 users,  load average: 0.31, 0.47, 0.38

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -46,14 +46,14 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   PTE page_table_target = page_table_base + vpn_0 * 4;
   word_t page_table_target_item = paddr_read(page_table_target, 4);
 
-  //if(PTE_V(page_table_target_item) == 0) assert(0);
+  if(PTE_V(page_table_target_item) == 0) assert(0);
 
   switch(type){
     case MEM_TYPE_IFETCH: if(PTE_X(page_table_target_item) == 0) assert(0);
       break;
     case MEM_TYPE_READ: if(PTE_R(page_table_target_item) == 0) assert(0);
       break;
-    case MEM_TYPE_WRITE: //if(PTE_W(page_table_target_item) == 0) assert(0);
+    case MEM_TYPE_WRITE: if(PTE_W(page_table_target_item) == 0) assert(0);
       break;
     default: assert(0); break;
   }
