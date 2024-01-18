b70eec79f9c8af2926f7e1b2bda2c0f4a8eb70b4
tracer-ics2023 <tracer@njuics.org>
2024-01-18 11:14:57 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:57 up 2 days, 22:44, 12 users,  load average: 0.25, 0.18, 0.18

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,5 +1,6 @@
 #include <memory.h>
-
+#include <stdint.h>
+#include <proc.h>//newly added for mm_brk
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
@@ -22,10 +23,45 @@ static void* pg_alloc(int n) {
 void free_page(void *p) {
   panic("not implement yet");
 }
-
+#ifdef HAS_VME
+extern PCB *current;
+extern uintptr_t load_file_break;
+#endif
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
+#ifdef HAS_VME
+  if(current->max_brk == 0){
+    Log("load_file_break: %p\n", (void *)load_file_break);
+    void * va_base;
+    if(load_file_break / PGSIZE == brk / PGSIZE){
+      va_base = (void *)ROUNDUP(brk,PGSIZE);
+      current->max_brk = (uintptr_t)(va_base);
+    }else{
+      Log("uheap_va before ROUNDDOWN: %p\n",(void *)brk);
+      va_base = (void *)ROUNDDOWN(brk,PGSIZE);
+      void * pa_base = pg_alloc(PGSIZE);
+      map(&current->as, va_base, pa_base, 0);
+      current->max_brk = (uintptr_t)(va_base + PGSIZE);
+    }
+    return 0;
+  }
+  if(brk > current->max_brk){
+    unsigned int gap = brk - current->max_brk;
+    unsigned int page_count = gap / PGSIZE;
+    if(page_count * PGSIZE == gap) page_count--;
+    for(int i = 0; i <= page_count; i++){
+      void * va_base = (void *)(current->max_brk + i * PGSIZE);
+      void * pa_base = pg_alloc(PGSIZE);
+      map(&current->as, va_base, pa_base, 0);
+    }
+    current->max_brk += (page_count + 1) * PGSIZE;
+  } else {
+    Log("current->max_brk: %p\n",(void *)current->max_brk);
+  }
   return 0;
+#else
+  return 0;
+#endif
 }
 
 void init_mm() {
