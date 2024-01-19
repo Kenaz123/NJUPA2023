9f768053cc2710ef4278ce1fd06fbf01b6bf93c2
tracer-ics2023 <tracer@njuics.org>
2024-01-19 19:06:43 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:06:43 up 4 days,  6:36, 12 users,  load average: 0.26, 0.58, 0.61

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -90,7 +90,7 @@ static void execute(uint64_t n) {
     IFDEF(CONFIG_DEVICE, device_update());
     word_t intr = isa_query_intr();
     if(intr != INTR_EMPTY){
-      cpu.pc = isa_raise_intr(intr, cpu.pc-4);
+      cpu.pc = isa_raise_intr(intr, cpu.pc);
     }
   }
 }
