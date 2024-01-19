b668988cadec5415f02935523a931c47716d5925
tracer-ics2023 <tracer@njuics.org>
2024-01-19 11:42:52 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:42:52 up 3 days, 23:12, 12 users,  load average: 0.46, 0.73, 0.82

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -30,7 +30,7 @@
 
 typedef vaddr_t PTE;
 
-paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
+/*paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //Log("[VA]: 0x%x", vaddr);
   word_t satp = cpu.satp;
   PTE page_dir_base = satp << 12;
@@ -65,5 +65,35 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t ppn = PTE_PPN(page_table_target_item) << 12;
   paddr_t paddr = ppn | offset;
   //assert(paddr == vaddr);
+  return paddr;
+}*/ 
+paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
+{
+  // // {
+  // printf("================\n");
+  // }
+#define SATP_MASK 0X3fffff
+#define PG_SHIFT 12
+#define PGT1_ID(val) (val >> 22)
+#define PGT2_ID(val) ((val & 0x3fffff) >> 12)
+#define OFFSET(val) (val & 0xfff)
+  word_t va_raw = (uint32_t)vaddr;
+  paddr_t *pt_1 = (paddr_t *)guest_to_host((paddr_t)(cpu.satp << PG_SHIFT));
+  // printf("pt1[id1(vaddr)] is 0x%x\n", pt_1[PGT1_ID(va_raw)]);
+  assert(pt_1 != NULL);
+  word_t *pt_2 = (word_t *)guest_to_host(pt_1[PGT1_ID(va_raw)]);
+  // if (vaddr == 0x7ffffded)
+
+  assert(pt_2 != NULL);
+  paddr_t paddr = (paddr_t)((pt_2[PGT2_ID(va_raw)] & (~0xfff)) | OFFSET(va_raw));
+  // if (vaddr >= 0x40000000 && vaddr <= 0x8000000)
+  // {
+  // printf("mmu translate vaddr %p\n", (void *)(long)vaddr);
+  // printf("translate ui is 0x%x, pt2 id is %x\n", pt_2[PGT2_ID(va_raw)] >> 12, PGT2_ID(va_raw));
+  // printf("mmu translate to paddr %p\n", (void *)(long)paddr);
+  // } // printf("vaddr is 0x%x, paddr is 0x%x\n", vaddr, paddr);
+  assert(vaddr >= 0x40000000 && vaddr <= 0xa1200000);
+  // assert(paddr == vaddr);
+  // return MEM_RET_FAIL;
   return paddr;
 }
