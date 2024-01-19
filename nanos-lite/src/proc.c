6b6fcb90673f10df8243392d88f4e5f315892354
tracer-ics2023 <tracer@njuics.org>
2024-01-19 16:01:00 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:00 up 4 days,  3:30, 12 users,  load average: 0.56, 0.52, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,6 +42,6 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  current = ((current == &pcb[0]) ? &pcb[0] : &pcb[0]);
+  current = ((current == &pcb[0]) ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
