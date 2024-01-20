caa0eeb2991453ba918c346a171d5a1ebb4a63a6
tracer-ics2023 <tracer@njuics.org>
2024-01-20 21:45:55 +0800

    >  compile NEMU 221900332 王卫东 Linux aifly-Inspiron-5488 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:55 up 5 days,  9:15, 12 users,  load average: 0.24, 0.39, 0.44

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,7 +5,7 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
-
+PCB *fg_pcb = NULL;
 void naive_uload(PCB *pcb, const char *filename);
 
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
@@ -27,14 +27,14 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  char *argv_nterm[] = {"/bin/bird", NULL};
- // char *argv_hello[] = {"/bin/hello", NULL};
+  char *argv_nterm[] = {"/bin/nterm", NULL};
+  char *argv_hello[] = {"/bin/hello", NULL};
   char *envp[] = {NULL};
-  context_kload(&pcb[1], hello_fun, "A");
-  context_uload(&pcb[0], "/bin/bird", argv_nterm, envp);
-  /*context_uload(&pcb[1], "/bin/nterm", argv_nterm, envp);
-  context_uload(&pcb[2], "/bin/nterm", argv_nterm, envp);*/
-  //context_uload(&pcb[3], "/bin/hello", argv_hello, envp);
+  //context_kload(&pcb[1], hello_fun, "A");
+  context_uload(&pcb[1], "/bin/nterm", argv_nterm, envp);
+  context_uload(&pcb[2], "/bin/nterm", argv_nterm, envp);
+  context_uload(&pcb[3], "/bin/nterm", argv_nterm, envp);
+  context_uload(&pcb[0], "/bin/hello", argv_hello, envp);
   //context_kload(&pcb[1], hello_fun, "B");
   switch_boot_pcb();
   //Log("Initializing processes...");
